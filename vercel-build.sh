68c78c2b13ce3eeb1c28e5893695ac5490e1eec9
Viktor Holovin <holovin@mail.com>
2025-08-03 15:51:04 +0200

    Add vercel.json and use native Hugo build

--- vercel-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/gohugoio/hugo/releases/download/v0.148.2/hugo_extended_0.148.2_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.148.2_Linux-64bit.tar.gz
-mv hugo /usr/local/bin/
-
-hugo
