8dac11f1fad1345a0da71f7282d4da30dfcdf6cb
Viktor Holovin <holovin@mail.com>
2025-08-03 15:45:42 +0200

    Vercel fix

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-HUGO_VERSION=0.148.2
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.148.2/hugo_extended_0.148.2_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.148.2_Linux-64bit.tar.gz
 mv hugo /usr/local/bin/
 
 hugo
